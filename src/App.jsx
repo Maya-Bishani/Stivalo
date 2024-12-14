e39909b4ec6974069174d72dcc5ef4f680efb8ee
Maya_Bishani <mayabishani6@gmail.com>
2024-12-14 19:34:30 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { BrowserRouter } from "react-router";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route, Link } from 'react-router-dom';
 import Navbar2 from './Component/Navbar/Navbar';
 import Footer from './Component/Footer/Footer';
 import HomePage from './Pages/HomePage/HomePage';
@@ -11,14 +9,9 @@ function App() {
   
   return (
     <>
-
-<Navbar2/>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-        </Routes>
+        <Navbar2/>
+        <HomePage/>
         <Footer/>
-    
-     
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename='/Stivalo'>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  </StrictMode>,
 )
+
